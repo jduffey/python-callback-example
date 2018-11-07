5b31ed84269a5c686aaa4422b33f95d9e3698322
Jed Duffey <jedduffey@gmail.com>
2018-11-07 15:01:21 -0500

    Minor refactor; add comment block

--- fibonacci_function.py
+++ fibonacci_function.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 import time
  
-def fibonacci(callbackFunction):
+def fibonacci(the_callback_function):
     values = []
     while(True):
         if len(values) < 2:
@@ -12,20 +12,26 @@ def fibonacci(callbackFunction):
         print(values)
         time.sleep(.25)
 
-        r = callbackFunction(values[-1])
+        '''
+        This is where we allow the primary function to run its
+        callback (secondary??) function
+        in such a way that the primary and "secondary" functions
+        can have their code separate.
+        '''
+        r = the_callback_function(values[-1], 17)
         if (r[0]):
             return(r[1])
 
-def check_17(valueToAssess):
-    if valueToAssess % 17 == 0:
-        return (True, valueToAssess)
+def check_if_div_by_x(argument, x):
+    if argument % x == 0:
+        return (True, argument)
 
-    if valueToAssess > 10000:
+    if argument > 10000:
         return (True, None)
 
     return (False,)
 
 if __name__ == '__main__':
-    result = fibonacci(check_17)
+    result = fibonacci(check_if_div_by_x)
     if (result != None):
         print(result)
